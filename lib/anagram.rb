5dacc8d4e49215d0b746e566188fd03f7cef574e
Janice Chan <janice.chan1288@gmail.com>
2022-06-23 13:09:44 -0400

    commit tree 13328140970824d23151632abd4fe416218ff768

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+class Anagram 
+    attr_reader :word # reader or accessor works
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |array_word|
+        array_word.split("").sort == word.split("").sort
+        end
+    end
+end
+
+# %w[enlists google inlets banana] same as: 
+# ["enlists", "google", "inlets", "banana"] array we pass in to match
+# "enlists".split("").sort
+# ["e", "i", "l", "n", "s", "s", "t"] 
+
+# .find wouldnt work bc want to return ALL matches. Find only returns one
+
+# one liner
+# def match(array)
+#     array.select { |array_word| array_word.split("").sort == @word.split("").sort }
+# end
+
